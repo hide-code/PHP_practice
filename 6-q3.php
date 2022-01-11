7f165567ab19c1364080ad469eda59e77cc197fd
hide-code <kitahide13@icloud.com>
2022-01-11 10:31:36 +0900

    レビュー２回目修正

--- 6-q3.php
+++ 6-q3.php
@@ -2,14 +2,12 @@
 //日時を加算変更するmodifyメソッド
 //指定したformatで返す
 $date = new DateTime('2021-03-02');
-echo $date->modify('-1 month')->format('Y-m-d')."\n";
+echo $date->modify('-1 month')->format('Y-m-d') . "\n";
 
-// $diffはDateIntervalクラスのインスタンス
-//　２つのふたつの DateTime オブジェクトの差を返す
+//diffはDateIntervalクラスのインスタンスで２つのふたつの DateTime オブジェクトの差を返す
 $date1 = new DateTime('2021-03-02');
 $date2 = new DateTime('1992-04-25');
-
 $diff = $date1->diff($date2);
 
-echo 'あの日から'.abs($diff->format('%R%a')).'日経過ました。';
+echo 'あの日から' . abs($diff->format('%R%a')) . '日経過ました。';
 
--- q9.php
+++ q9.php
@@ -15,11 +15,11 @@ $prefectures = [
 $kanto = ['東京', '神奈川', '栃木', '千葉', '茨城', '埼玉', '群馬'];
 
 foreach ($prefectures as $ken => $shi) {
-  
+
     if (in_array($ken, $kanto)){
         echo $ken . '県の県庁所在地は、' . $shi . 'です。' . "\n";
     } else {
-         echo $ken . '県は関東地方ではありません。' . "\n";
+        echo $ken . '県は関東地方ではありません。' . "\n";
     }
 }
 
